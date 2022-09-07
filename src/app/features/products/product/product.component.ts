4881ff9e36f50828e4d73bc6830d6b75ed2c314c
Olena Voitsekhivska <mirkbog@gmail.com>
2022-09-07 14:31:39 +0300

    OnPush fix

--- src/app/features/products/product/product.component.ts
+++ src/app/features/products/product/product.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent implements OnInit {
 
